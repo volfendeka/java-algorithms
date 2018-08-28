99cfcc83f45a5cc587cd1a5db41b3b3603ea5846
v.deka@mediawayss.com <v.deka@mediawayss.com>
2018-08-28 14:04:49 +0300

    karatsuba done

--- karatsuba/src/com/company/Main.java
+++ karatsuba/src/com/company/Main.java
@@ -1,13 +1,99 @@
 import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.Random;
+/*
+public class Main {
 
+    private static BigInteger counter [] = new BigInteger [2];
+
+    public static BigInteger karatsuba(BigInteger x, BigInteger y) {
+
+        // cutoff to brute force
+
+        int N = Math.max(x.bitLength(), y.bitLength());
+
+        //if (N <= 1) {
+        if (x.compareTo(BigInteger.valueOf(11)) < 0 & y.compareTo(BigInteger.valueOf(11)) < 0) {
+            return x.multiply(y);                // optimize this parameter
+        }
+
+        // number of bits divided by 2, rounded up
+        N = (N / 2) + (N % 2);
+
+        // x = a + 2^N b,   y = c + 2^N d
+        BigInteger b = x.shiftRight(N);
+        BigInteger a = x.subtract(b.shiftLeft(N));
+        BigInteger d = y.shiftRight(N);
+        BigInteger c = y.subtract(d.shiftLeft(N));
+
+        // compute sub-expressions
+        BigInteger ac    = karatsuba(a, c);
+        BigInteger bd    = karatsuba(b, d);
+        BigInteger abcd  = karatsuba(a.add(b), c.add(d));
+
+        BigInteger adbc = abcd.subtract(ac).subtract(bd);
+
+
+        //counter105;
+        if(adbc.equals(BigInteger.valueOf(8))){
+            counter[0] = BigInteger.valueOf(1);
+        }
+        //counter72;
+        if(adbc.equals(BigInteger.valueOf(7))){
+            counter[1] = BigInteger.valueOf(1);
+        }
+
+        BigInteger recurs = adbc.shiftLeft(N);
+
+        BigInteger result = ac.add(recurs).add(bd.shiftLeft(2*N));
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        long start, stop, elapsed;
+
+        String k = "44269423";
+        String z = "49823261";
+        String res = "2205647016448403";
+        //String k = "1685287499328328297814655639278583667919355849391453456921116729";
+        //String z = "7114192848577754587969744626558571536728983167954552999895348492";
+        System.out.println(k);
+        System.out.println(z);
+
+        BigInteger a = new BigInteger(k);
+        BigInteger b = new BigInteger(z);
+
+        BigInteger c = karatsuba(a, b);
+
+        BigInteger d = a.multiply(b);
+
+        System.out.println((c.equals(d)));
+
+        System.out.println(c);
+
+        printArray(counter);
+
+    }
+
+    private static void printArray(BigInteger[] anArray) {
+        for (int i = 0; i < anArray.length; i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(" ");
+            System.out.print(anArray[i]);
+        }
+    }
+}
+*/
 public class Main {
 
     public static String X;
     public static String Y;
-    public static int counter8;
-    public static int counter7;
+    public static int counter105 = 0;
+    public static int counter72 = 0;
+    public static int counter12 = 0;
 
     public static BigInteger karatsuba(String x, String y) {
 
@@ -49,20 +135,26 @@ public class Main {
         BigInteger bd = karatsuba(b, d);
         //long abcd = karatsuba((a+b), (c+d));
         BigInteger abcd = karatsuba((tempA.add(tempB)).toString(), (tempC.add(tempD)).toString());
-        BigInteger adbc = abcd.subtract(ac).subtract(bd);
+        abcd = abcd.subtract(ac).subtract(bd);
 
-        if(adbc.equals(BigInteger.valueOf(14))){
-            counter8++;
+        if(abcd.equals(BigInteger.valueOf(105))){
+            ++counter105;
         }
 
-        if(adbc.equals(BigInteger.valueOf(9))){
-            counter7++;
+        if(abcd.equals(BigInteger.valueOf(72))){
+            ++counter72;
         }
 
-        long kof1 = (long)Math.pow(10, (2*halfMax));
-        long kof2 = (long)Math.pow(10, halfMax);
+        if(abcd.equals(BigInteger.valueOf(12))){
+            ++counter12;
+        }
 
-        BigInteger formulaResult = ((ac.multiply(BigInteger.valueOf(kof1))).add((adbc.multiply(BigInteger.valueOf(kof2)))).add(bd));
+        BigInteger ten = BigInteger.valueOf(10);
+
+        BigInteger kof1 = ten.pow((2*halfMax));
+        BigInteger kof2 = ten.pow(halfMax);
+
+        BigInteger formulaResult = ((ac.multiply(kof1)).add((abcd.multiply(kof2))).add(bd));
 
         return formulaResult;
 
@@ -72,13 +164,14 @@ public class Main {
 
         String res = "71332574014261268360454523927286";
 
-        String a = "3957322621234423";
-        String b = "7748313756335578";
+        String a = "1685287499328328297814655639278583667919355849391453456921116729";
+        String b = "7114192848577754587969744626558571536728983167954552999895348492";
 
         BigInteger c = karatsuba(a, b);
 
-        System.out.println(counter8);
-        System.out.println(counter7);
+        System.out.println(counter105);
+        System.out.println(counter72);
+        System.out.println(counter12);
         System.out.println(c);
 
         BigInteger l = new BigInteger(a);
