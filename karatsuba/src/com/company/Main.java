9950c89efdf6f1b1fea0fcad88bfbec5c7bcbd72
v.deka@mediawayss.com <v.deka@mediawayss.com>
2018-08-28 13:01:53 +0300

    remove comment

--- karatsuba/src/com/company/Main.java
+++ karatsuba/src/com/company/Main.java
@@ -1,92 +1,7 @@
 import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.Random;
-/*
-public class Main {
 
-    private static BigInteger counter [] = new BigInteger [2];
-
-    public static BigInteger karatsuba(BigInteger x, BigInteger y) {
-
-        // cutoff to brute force
-
-        int N = Math.max(x.bitLength(), y.bitLength());
-
-        //if (N <= 1) {
-        if (x.compareTo(BigInteger.valueOf(11)) < 0 & y.compareTo(BigInteger.valueOf(11)) < 0) {
-            return x.multiply(y);                // optimize this parameter
-        }
-
-        // number of bits divided by 2, rounded up
-        N = (N / 2) + (N % 2);
-
-        // x = a + 2^N b,   y = c + 2^N d
-        BigInteger b = x.shiftRight(N);
-        BigInteger a = x.subtract(b.shiftLeft(N));
-        BigInteger d = y.shiftRight(N);
-        BigInteger c = y.subtract(d.shiftLeft(N));
-
-        // compute sub-expressions
-        BigInteger ac    = karatsuba(a, c);
-        BigInteger bd    = karatsuba(b, d);
-        BigInteger abcd  = karatsuba(a.add(b), c.add(d));
-
-        BigInteger adbc = abcd.subtract(ac).subtract(bd);
-
-
-        //counter105;
-        if(adbc.equals(BigInteger.valueOf(8))){
-            counter[0] = BigInteger.valueOf(1);
-        }
-        //counter72;
-        if(adbc.equals(BigInteger.valueOf(7))){
-            counter[1] = BigInteger.valueOf(1);
-        }
-
-        BigInteger recurs = adbc.shiftLeft(N);
-
-        BigInteger result = ac.add(recurs).add(bd.shiftLeft(2*N));
-
-        return result;
-    }
-
-    public static void main(String[] args) {
-        long start, stop, elapsed;
-
-        String k = "44269423";
-        String z = "49823261";
-        String res = "2205647016448403";
-        //String k = "1685287499328328297814655639278583667919355849391453456921116729";
-        //String z = "7114192848577754587969744626558571536728983167954552999895348492";
-        System.out.println(k);
-        System.out.println(z);
-
-        BigInteger a = new BigInteger(k);
-        BigInteger b = new BigInteger(z);
-
-        BigInteger c = karatsuba(a, b);
-
-        BigInteger d = a.multiply(b);
-
-        System.out.println((c.equals(d)));
-
-        System.out.println(c);
-
-        printArray(counter);
-
-    }
-
-    private static void printArray(BigInteger[] anArray) {
-        for (int i = 0; i < anArray.length; i++) {
-            if (i > 0) {
-                System.out.print(", ");
-            }
-            System.out.print(" ");
-            System.out.print(anArray[i]);
-        }
-    }
-}
-*/
 public class Main {
 
     public static String X;
@@ -110,7 +25,7 @@ public class Main {
             long Yvalue = Long.parseLong(Y);
             if(Xvalue < 10 && Yvalue < 10){
                 long res = Xvalue * Yvalue;
-                
+
                 BigInteger result = BigInteger.valueOf(res);
                 return result;
             }
